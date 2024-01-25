b0e6fa29dd253ab63503b2ee95b426e548ae8578
Allison Shiozawa <allishiozawa@gmail.com>
2024-01-25 15:02:43 -0300

    Graphic Intel Applied

--- main.js
+++ main.js
@@ -1,31 +1,39 @@
 import { generateReturnsArray } from "./src/investmentGoals";
+import { Chart } from "chart.js/auto";
 
+const finalMoneyChart = document.getElementById("final-money-distribution");
+const progressionChart = document.getElementById("progression");
 const form = document.getElementById("investment-form");
 //const calculateButton = document.getElementById('calculate-results');
 const clearFormButton = document.getElementById("clear-form");
+let doughnutChartReference = {};
+let progressionChartReference = {};
+
+function formatCurrency(value) {
+  return value.toFixed(2);
+}
 
 function renderProgression(evt) {
   evt.preventDefault();
   if (document.querySelector(".error")) {
     return;
   }
+  resetCharts();
   // const startingAmount = Number(form['startingAmount'].value);
   const startingAmount = Number(
     document.getElementById("starting-amount").value.replace(",", ".")
   );
   const additionalContribution = Number(
-    document
-      .getElementById("additional-contribution")
-      .value.value.replace(",", ".")
+    document.getElementById("additional-contribution").value.replace(",", ".")
   );
   const timeAmount = Number(document.getElementById("time-amount").value);
   const timeAmountPeriod = document.getElementById("time-amount-period").value;
   const returnRate = Number(
-    document.getElementById("return-rate").value.value.replace(",", ".")
+    document.getElementById("return-rate").value.replace(",", ".")
   );
   const returnRatePeriod = document.getElementById("evaluation-period").value;
   const taxRate = Number(
-    document.getElementById("tax-rate").value.value.replace(",", ".")
+    document.getElementById("tax-rate").value.replace(",", ".")
   );
 
   const returnsArray = generateReturnsArray(
@@ -36,8 +44,82 @@ function renderProgression(evt) {
     returnRate,
     returnRatePeriod
   );
+  const finalInvestmentObject = returnsArray[returnsArray.length - 1];
 
-  console.log(returnsArray);
+  doughnutChartReference = new Chart(finalMoneyChart, {
+    type: "doughnut",
+    data: {
+      labels: ["Total Invested", "Return Rates", "Taxes on Profit"],
+      datasets: [
+        {
+          label: "My First Dataset",
+          data: [
+            formatCurrency(finalInvestmentObject.investedAmount),
+            formatCurrency(
+              finalInvestmentObject.totalInterestReturns * (1 - taxRate / 100)
+            ),
+            formatCurrency(
+              finalInvestmentObject.totalInterestReturns * (taxRate / 100)
+            ),
+          ],
+          backgroundColor: [
+            "rgb(255, 99, 132)",
+            "rgb(54, 162, 235)",
+            "rgb(255, 205, 86)",
+          ],
+          hoverOffset: 4,
+        },
+      ],
+    },
+  });
+
+  progressionChartReference = new Chart(progressionChart, {
+    type: "bar",
+    data: {
+      labels: returnsArray.map((investmentObject) => investmentObject.month),
+      datasets: [
+        {
+          label: "Total Invested",
+          data: returnsArray.map((investmentObject) =>
+            formatCurrency(investmentObject.investedAmount)
+          ),
+          backgroundColor: "rgb(255, 99, 132)",
+        },
+        {
+          label: "Return on Investment",
+          data: returnsArray.map((investmentObject) =>
+            formatCurrency(investmentObject.interestReturns)
+          ),
+          backgroundColor: "rgb(54, 162, 235)",
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      scales: {
+        x: {
+          stacked: true,
+        },
+        y: {
+          stacked: true,
+        },
+      },
+    },
+  });
+}
+
+function isObjectEmpty(obj) {
+  return Object.keys(obj).length === 0;
+}
+
+function resetCharts() {
+  if (
+    !isObjectEmpty(doughnutChartReference) &&
+    !isObjectEmpty(progressionChartReference)
+  ) {
+    doughnutChartReference.destroy();
+    progressionChartReference.destroy();
+  }
 }
 
 function clearForm() {
@@ -46,6 +128,8 @@ function clearForm() {
   form["time-amount"].value = "";
   form["return-rate"].value = "";
   form["tax-rate"].value = "";
+
+  resetCharts();
 
   const errorInputContainers = document.querySelectorAll(".error");
   for (const errorInputContainer of errorInputContainers) {
